a48da741b6a1833292f8495afbb9b1d2883d8e1e
PARAG <PARAG@DESKTOP-52B7LN8>
2022-07-12 18:54:07 +0530

    Second Commit

---
+++ Day1_GitHub/src/com/capgemini/intro/Program2.java
@@ -0,0 +1,12 @@
+package com.capgemini.intro;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5; 
+		int b = 5;
+		System.out.println(a+b);
+	}
+
+}
