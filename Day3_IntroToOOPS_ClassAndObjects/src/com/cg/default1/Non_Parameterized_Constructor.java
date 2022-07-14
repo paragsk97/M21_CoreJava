12b521872977d433e41f0bf9989d0790bf8fe885
PARAG <PARAG@DESKTOP-52B7LN8>
2022-07-14 13:18:47 +0530

    First Commit

---
+++ Day3_IntroToOOPS_ClassAndObjects/src/com/cg/default1/Non_Parameterized_Constructor.java
@@ -0,0 +1,17 @@
+package com.cg.default1;
+
+class A
+{
+	A()
+	{
+		System.out.println("Hey There, I am using Constructor");
+	}
+}
+public class Non_Parameterized_Constructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A obj = new A();
+	}
+
+}
