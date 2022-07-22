36ed6c8c3df536b2e1363a4927471579e2c41080
PARAG <PARAG@DESKTOP-52B7LN8>
2022-07-22 10:33:18 +0530

    First Commit

---
+++ Day9_Interfaces&WrapperClasses/src/com/cg/annoclass/Demo.java
@@ -0,0 +1,21 @@
+package com.cg.annoclass;
+
+abstract class Annonymus
+{
+	public abstract void method();
+}
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Annonymus a = new Annonymus()
+		{
+			public void method()
+			{
+			System.out.println("This is an example of Annonymus class");
+			}
+		};
+	a.method();
+	}
+
+}
---
+++ Day9_Interfaces&WrapperClasses/src/com/cg/interfaces/Test.java
@@ -0,0 +1,9 @@
+package com.cg.interfaces;
+
+class Test
+{
+	public static void main(String args[]) {
+		X d = new X();
+		d.display1();
+	}
+}
