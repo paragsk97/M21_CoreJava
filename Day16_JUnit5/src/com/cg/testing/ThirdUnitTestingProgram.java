1831ead948c5b683dff83d2d0122aa3c23ea13fe
PARAG <PARAG@DESKTOP-52B7LN8>
2022-07-29 12:05:04 +0530

    First Commit

---
+++ Day16_JUnit5/src/com/cg/testing/ThirdUnitTestingProgram.java
@@ -0,0 +1,21 @@
+package com.cg.testing;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ThirdUnitTestingProgram {
+
+	@Test
+	@DisplayName("Test is Successfully Implemented")
+	void function()
+	{
+		System.out.println("Want to display a name");
+	}
+	@Test
+	@Disabled("Not Implemented")
+	void function1()
+	{
+		System.out.println("Fails");
+	}
+}
