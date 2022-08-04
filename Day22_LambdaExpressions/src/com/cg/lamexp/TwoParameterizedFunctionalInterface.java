ce6ed0b75d8b3ed4397da06756f6cca6afec1d3e
PARAG <PARAG@DESKTOP-52B7LN8>
2022-08-04 12:56:36 +0530

    First Commit

---
+++ Day22_LambdaExpressions/src/com/cg/lamexp/TwoParameterizedFunctionalInterface.java
@@ -0,0 +1,22 @@
+package com.cg.lamexp;
+
+interface E
+{
+	String show(String s,String t);
+}
+
+public class TwoParameterizedFunctionalInterface {
+
+	public static void main(String[] args) {
+		
+		//Lambda Expression
+		E obj= (String s,String t)->
+		{
+			return s+t;
+		};
+		System.out.println(obj.show("Parag"," " + "Sambhaji"));
+
+	}
+
+	}
+
